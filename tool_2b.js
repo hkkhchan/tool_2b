638d4ec5a726b91e532410d9326f12cbd57cd71b
hkkhchan <hkkhchan@gmail.com>
2016-10-18 01:01:05 +0800

    complete reg

--- tool_2b.js
+++ tool_2b.js
@@ -25,11 +25,11 @@ toolApp.controller('toolAppCtrl', function($scope, ngDialog){
   $scope.set_selected=function(d){$scope.mode=d;} 
   $scope.openDialog= function(){
     var dialog='<div>';
-    dialog+='<h2>"繁簡轉換"的介紹</h2>';
-    dialog+='<p>我將繁簡兩種文字的unicode放在兩個object，用簡單的coding便能將繁簡字轉換。資料來源是來自<a href="http://bunbun000.com/home/word/">笨笨網站</a>。</p>';
+    dialog+='<h2>"刪除備註"的介紹</h2>';
+    dialog+='<p>這個工具利用了regular expression將備註刪除</p>';
     dialog+='<p>本頁完全不用JQuery來處理DOM，用AngularJS來將結果呈現出來。</p>';
     dialog+='<p>AngularJS能給予html資料源作雙向綁定，所以無須綁定元素的事件，使Coding看起來簡潔。</p>';
-    dialog+='<p>我曾試過用php來寫，即是將轉換編碼的部份交給php處理。發覺一旦是海量文字便處理得很慢。</p>';
+    dialog+='<p>此版面的代碼，我也放到<a href="//github.com/hkkhchan/tool_2b">GitHub</a>，歡迎參觀。</p>';
     dialog+='</div>';
     ngDialog.open({
       template: dialog,
