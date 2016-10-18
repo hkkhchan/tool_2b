c9f5c64a2e7079666f6c500ba7292c8015bb0972
hkkhchan <hkkhchan@gmail.com>
2016-10-18 20:43:15 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # tool_2b
+## 功能: 刪除備註
+## 網址: [http://dp-kkhchan1.rhcloud.com](http://dp-kkhchan1.rhcloud.com)
+  
+  
+### 刪除備註的介紹  
+這個工具利用了regular expression將備註刪除  
+  
+### demo網站使用的skillset  
+AngularJS/Drupal
--- index.php
+++ index.php
@@ -1,11 +1,17 @@
+<link href="ngDialog.min.css" rel="stylesheet">
+<link href="ngDialog-theme-default.min.css" rel="stylesheet">
+<link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="tool.css" rel="stylesheet" type="text/css" />
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
+<script src="ngDialog.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
 <script src="tool_2b.js"></script>
 <section ng-app="toolApp" ng-controller="toolAppCtrl">
 <article>
   <nav>
-    <span id="toDef" class="option active" ng-class="is_selected(0)" ng-click="set_selected(0)">刪除HTML備註</span>
-    <span id="toSC" class="option" ng-class="is_selected(1)" ng-click="set_selected(1)">刪除JS/PHP備註</span>
-    <span id="toTC" class="option" ng-class="is_selected(2)" ng-click="set_selected(2)">刪除Python備註</span>
+    <span id="toDef" class="option active" ng-class="is_selected(0)" ng-click="set_selected(0)">HTML</span>
+    <span id="toSC" class="option" ng-class="is_selected(1)" ng-click="set_selected(1)">JS/PHP</span>
+    <span id="toTC" class="option" ng-class="is_selected(2)" ng-click="set_selected(2)">Python</span>
     <span type="button" class="button" ng-click="openDialog()">介紹</span>
   </nav>
 </article>
